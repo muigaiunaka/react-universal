30931b6987c4b109e8104f5c5e74f6c046bf69bf
Muigai <muigaiunaka@gmail.com>
2018-05-06 23:40:48 -0400

    frontend: react utils, react router routes and updated index

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './components/App';
-const render = (Component) => {
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import history from './utils/history'
+import { Router, Route } from 'react-router';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './redux/reducers';
+import { AUTH_USER } from './redux/actions/types';
+import Routes from './Routes';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+// contains the redux state
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if there is a token, consider the user signed in
+if (token) {
+  // update application state
+  store.dispatch({ type: AUTH_USER });
+}
+const render = () => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Provider store={store}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+      </Provider>
     </AppContainer>,
     document.getElementById('root'),
   );
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js').catch(() => {});
+      navigator.serviceWorker.register('service-worker.js').catch(() => { });
     });
   }
 };
-render(App);
+render();
 if (module.hot) {
   module.hot.accept();
 }
