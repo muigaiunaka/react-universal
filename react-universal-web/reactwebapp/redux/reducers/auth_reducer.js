b388ca46a23ebf49c6b1d192c4a47a1cda939493
muigaiunaka <muigaiunaka@gmail.com>
2018-06-15 02:36:18 -0400

    refactored, added some methods to be moved later (to user reducer)

--- react-universal-web/reactwebapp/redux/reducers/auth_reducer.js
+++ react-universal-web/reactwebapp/redux/reducers/auth_reducer.js
@@ -3,6 +3,9 @@ import {
     UNAUTH_USER,
     AUTH_ERROR,
     FETCH_USER,
+    ADMIN_USER,
+    FETCH_USERS,
+    DELETE_USER
 } from '../actions/types'
 
 export default function(state = {}, action) {
@@ -15,6 +18,12 @@ export default function(state = {}, action) {
             return { ...state, error: action.payload}
         case FETCH_USER:
             return { ...state, user: action.payload }
+        case ADMIN_USER:
+            return { ...state, role: action.payload }
+        case FETCH_USERS:
+            return { ...state, users: action.payload }
+        case DELETE_USER:
+            return { response: action.payload }
         default: 
             return state;
     }
