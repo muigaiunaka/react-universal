eb770467e83e9f50cf290b46baea5ba53bf634e3
muigaiunaka <muigaiunaka@gmail.com>
2018-06-15 02:33:55 -0400

    updated actions and types

--- react-universal-web/reactwebapp/redux/actions/index.js
+++ react-universal-web/reactwebapp/redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import history from '../../utils/history';
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_USER } from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_USER, ADMIN_USER, FETCH_USERS, DELETE_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3005';
 
@@ -16,7 +16,7 @@ export function signinUser({ email, password }) {
             // redirect to the route that is protected
             history.push('/profile');
         } catch(error) {
-            dispatch(authError('Bad Sign in Info'));
+            dispatch(authError('We did not recognize that email or password. Try again'));
         }
     }
 }
@@ -55,7 +55,7 @@ export function signoutUser() {
 export function fetchUser() {
     return async (dispatch) => {
         try {
-            const response = await axios.get(`${ROOT_URL}`, {
+            const response = await axios.get(`${ROOT_URL}/user`, {
                 // pass in headers as option of request
                 headers: { authorization: localStorage.getItem('token')}
             });
@@ -63,6 +63,62 @@ export function fetchUser() {
                 type: FETCH_USER,
                 payload: response.data.user
             })
+        } catch(error) {
+
+        }
+    }
+}
+
+export function isAdmin() {
+    return async (dispatch) => {
+        try {
+            const response = await axios.get(`${ROOT_URL}/admin`, {
+                // pass in headers as option of request
+                headers: { authorization: localStorage.getItem('token')}
+            });
+            dispatch({
+                type: ADMIN_USER,
+                payload: response.data.role
+            })
+        } catch(error) {
+
+        }
+    }
+}
+
+export function fetchUsers() {
+    return async (dispatch) => {
+        try {
+            const response = await axios.get(`${ROOT_URL}/users`, {
+                // pass in headers as option of request
+                headers: { authorization: localStorage.getItem('token')}
+            });
+            dispatch({
+                type: FETCH_USERS,
+                payload: response.data.users
+            })
+        } catch(error) {
+
+        }
+    }
+}
+
+export function deleteUser() {
+    return async (dispatch) => {
+        try {
+            const response = await axios.delete(`${ROOT_URL}/user`, {
+                // pass in headers as option of request
+                headers: { authorization: localStorage.getItem('token')}
+            });
+            dispatch({
+                type: DELETE_USER,
+                payload: response.data.response
+            })
+            localStorage.removeItem('token')
+            history.push('/');
+            return {
+                type: UNAUTH_USER,
+            }
         } catch(error) {
 
         }
--- react-universal-web/reactwebapp/redux/actions/types.js
+++ react-universal-web/reactwebapp/redux/actions/types.js
@@ -2,3 +2,6 @@ export const AUTH_USER = 'auth_user';
 export const UNAUTH_USER = 'unauth_user';
 export const AUTH_ERROR = 'auth_error';
 export const FETCH_USER = 'fetch_user';
+export const ADMIN_USER = 'admin_user';
+export const FETCH_USERS = 'fetch_users';
+export const DELETE_USER = 'delete_user'
