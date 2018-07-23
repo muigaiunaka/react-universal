91b13b0609e3e5b64d5cb4d4fabd00bbdee9c6e7
muigaiunaka <muigaiunaka@gmail.com>
2018-07-23 14:02:29 -0400

    toughened up security with envi vars

--- react-universal-node-server/server.js
+++ react-universal-node-server/server.js
@@ -1,48 +1,53 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const helmet = require('helmet');
-const session = require('express-session');
-const compression = require('compression');
-const router = require('./routes');
-const cors = require('cors');
-const config = require('./config');
+require("dotenv").config()
+
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const helmet = require("helmet");
+const session = require("express-session");
+const compression = require("compression");
+const router = require("./routes");
+const cors = require("cors");
+const config = require("./config");
+const passport = require('passport');
 
 let app = express();
 
 const EXPIRYDATE = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
-const PORT = process.env.PORT || 3005;
-const SERVER = process.env.SERVER || '127.0.0.1:27017';
-const DATABASE = process.env.DATABASE || 'react-universal';
+const PORT = process.env.PORT;
+const SERVER = process.env.SERVER;
+const DATABASE = process.env.DATABASE;
 /* APP SETUP */
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors(config.corsOptions)); // TODO: define 
 // Apply body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(passport.initialize())
 app.use(helmet())
 app.use(compression());
-app.set('trust proxy', 1) // trust first proxy
-app.set('json spaces', 2); // for prettify in express 4
+app.set("trust proxy", 1) // trust first proxy
+app.set("json spaces", 2); // for prettify in express 4
 app.use(session({
-  secret: 's3Cur3',
-  name: 'sessionC1-10P',
+  secret: "s3Cur3",
+  name: "sessionC1-10P",
   resave: true,
   saveUninitialized: true,
   cookie: {
     secure: true,
     httpOnly: true,
-    //   domain: '',
-    //   path: '',
+    //   domain: "",
+    //   path: "",
     expires: EXPIRYDATE
   }
 }))
+
 router(app);
 
 module.exports = app
@@ -52,6 +57,7 @@ mongoose.connect(`mongodb://${SERVER}/${DATABASE}`)
 .then(() => {
   app.listen(PORT);
   console.log(`Server running on port ${PORT}, no matter what you say or what you do`);
+  console.log(process.env.PORT)
 })
 .catch((e) => {
   console.log(e);
