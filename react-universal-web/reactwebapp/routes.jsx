f9fbb3da5b36ca6195520de23f9eb7e259592b12
muigaiunaka <muigaiunaka@gmail.com>
2018-07-23 14:11:16 -0400

    updated dependencies

--- react-universal-web/reactwebapp/routes.jsx
+++ react-universal-web/reactwebapp/routes.jsx
@@ -7,16 +7,19 @@ import RequireAuth from './components/Auth/RequireAuth';
 import SignIn from './components/Auth/SignIn';
 import SignOut from './components/Auth/SignOut';
 import SignUp from './components/Auth/SignUp';
+import StyleGuide from './components/StyleGuide/StyleGuide';
+import SplitLayout from './components/SplitLayout/SplitLayout';
 
 const Routes = () => (
     <div>
-        <AuthLayout exact path="/" component={Landing}/>
-        <AuthLayout path="/signin" component={SignIn}/>
-        <AuthLayout path="/signout" component={SignOut}/>
-        <AuthLayout path="/signup" component={SignUp}/>
+        <AuthLayout exact path="/" component={SplitLayout(Landing)}/>
+        <AuthLayout path="/signin" component={SplitLayout(SignIn)}/>
+        <AuthLayout path="/signout" component={SplitLayout(SignOut)}/>
+        <AuthLayout path="/signup" component={SplitLayout(SignUp)}/>
         {/* RequireAuth: Higher Order Component requiring user to be authenticated (aka signed in) */}
-        <AuthLayout path="/profile" component={RequireAuth(Profile)}/>
+        <AuthLayout path="/profile" component={RequireAuth(SplitLayout(Profile))}/>
         <AuthLayout path="/admin" component={RequireAuth(Admin)}/>
+        <AuthLayout path="/style-guide" component={StyleGuide}/>
     </div>
 )
 
